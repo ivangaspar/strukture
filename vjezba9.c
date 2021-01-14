d55e7cec1222259144e86f7b5950fbc5de918664
Ivangaspar01 <igaspa02@fesb.hr>
2021-01-14 14:26:16 +0100

    Create vjezba9.c

---
+++ vjezba9.c
@@ -0,0 +1,158 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+
+struct Cvor;
+typedef struct Cvor *Poz;
+
+struct Cvor
+{
+	int El;
+	Poz Desno;
+	Poz Lijevo;
+
+};
+
+Poz UmetniBroj(Poz, int);
+void Print(Poz);
+Poz Obrisi(Poz, int);
+Poz Najmanji(Poz);
+void Pronadji(Poz, int);
+
+
+int main()
+{
+	int izbor = 1, broj = 0;
+	Poz root = NULL;
+
+	while (izbor)
+	{
+		puts("unesite izbor:1-unos elementam, 2-print ,3-obrisati element,4-pronaci element, 5-exit");
+		scanf("%d", &izbor);
+
+		switch (izbor)
+		{
+		case 1:
+			puts("odaberite broj ");
+			scanf("%d", &broj);
+			root = UmetniBroj(root, broj);
+			break;
+		case 2:
+			puts("elementi su: ");
+			Print(root);
+			break;
+		case 3:
+			puts("unesite broj koji zelite izbrisati: ");
+			scanf("%d", &broj);
+			root = Obrisi(root, broj);
+			break;
+		case 4:
+			puts("unesite broj koji zelite pronaci: ");
+			scanf("%d", &broj);
+			Pronadji(root, broj);
+			break;
+		case 5:
+			izbor = 0;
+			break;
+		default:
+			printf("kriva naredba \n");
+			break;
+		}
+
+
+	}
+
+
+	return 0;
+}
+
+Poz UmetniBroj(Poz P, int n)
+{
+	if (P == NULL)
+	{
+		P = (Poz)malloc(sizeof(struct Cvor));
+		P->El = n;
+		P->Desno = NULL;
+		P->Lijevo = NULL;
+
+	}
+	else if (P->El < n)
+		P->Desno = UmetniBroj(P->Desno, n);
+	else if (P->El > n)
+		P->Lijevo = UmetniBroj(P->Lijevo, n);
+
+	return P;
+}
+
+void Print(Poz P)
+{
+	if (P != NULL)
+	{
+		printf("%d", P->El);
+		Print(P->Lijevo);
+		Print(P->Desno);
+
+	}
+
+}
+
+Poz Obrisi(Poz P, int n)
+{
+	Poz tmp = NULL;
+
+	if (P->El > n)
+		P->Lijevo = Obrisi(P->Lijevo, n);
+	else if (P->El < n)
+		P->Desno = Obrisi(P->Desno, n);
+	else
+	{
+		if (P->Desno != NULL && P->Lijevo != NULL)
+		{
+			tmp = Najmanji(P->Desno);
+			P->El = tmp->El;
+			P->Desno = Obrisi(P->Desno, tmp->El);
+		}
+
+		else
+		{
+			tmp = P;
+			if (P->Lijevo == NULL)
+				P = P->Desno;
+			else
+				P = P->Lijevo;
+			free(tmp);
+
+		}
+	}
+
+
+
+	return P;
+
+}
+
+Poz Najmanji(Poz P)
+{
+	if (P != NULL && P->Lijevo != NULL)
+		return Najmanji(P->Lijevo);
+
+	return P;
+
+}
+
+void Pronadji(Poz P, int n)
+{
+	if (P->El < n)
+		Pronadji(P->Desno, n);
+	else if (P->El > n)
+		Pronadji(P->Lijevo, n);
+	else if (P->El == n)
+		puts("element je pronadjen ");
+	else
+		puts("taj element ne postoji ");
+
+
+}
